e4082649adefca35c3b42aaf18e707cf3bb12110
Mike Falk <falk478@yahoo.com>
2021-01-21 15:56:03 -0800

    Add project files.

---
+++ LAB09-LINQ/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace LAB09_LINQ
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
