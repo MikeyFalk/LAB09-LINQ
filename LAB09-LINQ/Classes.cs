d039eaea0c04d55bf0a2eb6a6f3276d19e6a5f19
Mike Falk <falk478@yahoo.com>
2021-01-21 22:48:48 -0800

    wrote some shit down doesn't fucking work. Why can't we actually cove this shit in class so I'm not wasting hours of my time going in circles to just turn more really crappy shit?

---
+++ LAB09-LINQ/Classes.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LAB09_LINQ
+{
+    class Classes
+    {
+        public string type { get; set; }
+        public IList<double> coordinates { get; set; }
+    }
+
+    public class Properties
+    {
+        public string zip { get; set; }
+        public string city { get; set; }
+        public string state { get; set; }
+        public string address { get; set; }
+        public string borough { get; set; }
+        public string neighborhood { get; set; }
+        public string county { get; set; }
+    }
+
+    public class Feature
+    {
+        public string type { get; set; }
+        public Geometry geometry { get; set; }
+        public Properties properties { get; set; }
+    }
+
+    public class Example
+    {
+        public string type { get; set; }
+        public IList<Feature> features { get; set; }
+    }
+}
